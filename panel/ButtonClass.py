06df72841eab55242fbc695ad4e2934b283a0c99
austinlculver <austinlculver@gmail.com>
2014-09-23 00:59:55 +0000

    DA CODE

---
+++ panel/ButtonClass.py
@@ -0,0 +1,34 @@
+import pygame
+
+class button:
+
+	def __init__(self):
+		self.name = "push me"
+		self.position = (80,120,80,20)
+		self.color = (150,36,7)
+		self.font = pygame.font.Font(None,20)
+		self.label = self.font.render(self.name, 1, (255,0,255))
+		self.mask = None
+		
+		
+	
+	def draw(self,screen):
+		self.mask = pygame.draw.rect(screen,self.color,self.position)
+		screen.blit(self.label,(self.position[0], self.position[1]))
+		
+	def press(self):
+		self.name = "ow"
+		self.color = (255,255,255)
+		self.label = self.font.render(self.name, 1, (0,255,0))
+		
+	def unpress(self):
+		self.name = "push me"
+		self.position = (80,120,80,20)
+		self.color = (150,36,7)
+		self.font = pygame.font.Font(None,20)
+		self.label = self.font.render(self.name, 1, (255,0,255))
+		self.mask = None
+		
+	def updatePosition(speed):
+		self.position[0] += speed[0]
+		self.position[1] += speed[1]
---
+++ panel/Sender.py
@@ -0,0 +1,24 @@
+import json
+import sys
+import pygame
+import random
+from ClassTutorial import button
+
+
+pygame.init()
+screen = pygame.display.set_mode((600,400))
+b = button()
+while 1:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			quit(None)
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			mouse = pygame.mouse.get_pos()
+			if b.mask.collidepoint(mouse):
+				b.press()
+		elif event.type == pygame.MOUSEBUTTONUP:
+			b.unpress()
+	
+	screen.fill((36,150,7))
+	b.draw(screen)
+	pygame.display.update()
