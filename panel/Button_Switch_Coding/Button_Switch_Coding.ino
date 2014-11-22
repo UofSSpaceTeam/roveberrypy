dddfa3feb5d8962912d1ae1881be12515422a20f
jakespilchuk <jakespilchuk@gmail.com>
2014-11-22 18:49:34 +0000

    This is the coding for one switch. Nothing has been implemented for several switches as of yet.

---
+++ panel/Button_Switch_Coding/Button_Switch_Coding.ino
@@ -0,0 +1,28 @@
+int inPinOne = 2;
+int stateOne = 0;
+int newstateOne;
+
+void setup() {
+  pinMode(inPinOne,INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int readingOne = digitalRead(inPinOne);
+  if (readingOne == HIGH)
+  {
+    newstateOne = 0;
+  }
+  else
+  {
+    newstateOne = 1;
+  }
+  if (newstateOne != stateOne)
+  {
+    delay(100);
+    Serial.print("Switch One:");
+    Serial.println(newstateOne);
+    Serial.println("===============");
+  }
+  stateOne = newstateOne;
+}
