d81c92ed0ecf33e75ac3dcf352f5eb2afec173e9
vpeng <vpeng@usst.ca>
2014-11-23 17:07:31 +0000

    Significant Changes to BaseApp - In order for Kivy to play nice with threads and queues in a relibable way - it needed to be the base thread - Otherwise it seems to randomly crash due to its use of OpenGL for drawing interface items
    This has some MAJOR consequences
    - All threads are created during initialization of Kivy GUI App and are part of it
    - Threaded actions for the GUI itself need to be done with the kivy.clock object
    - Only one window is possible
    - Pygame needs to get unloaded first (This may conflict with Communications threads - not sure)
    
    From my testing it seems that all of the threads work properly and the mailboxes/queues managed by the communication thread works properly.
    
    PS: In order to use Kivy video properly I had to repackage it. Please download it and follow instructions on the USST Google Drive under design\software.
    
    I am open to discussion on choosing a different GUI toolkit but it seems like all the major ones need to be a base thread.

--- base/baseApp.py
+++ base/baseApp.py
@@ -1,55 +1,100 @@
 #Import all of the thread modules
 from threads.communicationThread import communicationThread
-from threads.guiThread import guiThread
 from threads.inputThread import inputThread
 from threads.navigationThread import navigationThread
 from threads.panelThread import panelThread
 
+#Import modules needed for GUI communication
+import baseMessages
+import json
+from Queue import Queue
 import time
+import threads.unicodeConvert
 
-def startThreads():
-	commThread.start()
-	inputThread.start()
-	navThread.start()
-	panelThread.start()
-	guiThread.start()
+#imports for Kivy GUI
+import kivy
+#Import modules needed to load the kv language file
+from kivy.app import App
+from kivy.lang import Builder
+#Turn off fullscreen - alternatively use 'fake' for borderless
+from kivy.config import Config
+Config.set('graphics', 'fullscreen', '0')
+#Import modules needed to make a window (Resizeable)
+from kivy.core.window import Window
+Window.size = (800,500)
+#Import scheduler
+from kivy.clock import Clock
 
-def stopThreads():
-	commThread.stop()
-	inputThread.stop()
-	navThread.stop()
-	panelThread.stop()
-	guiThread.stop()
+convert = threads.unicodeConvert.convert
+	
+class KivyGuiApp(App):
+	def build(self):
+		#Set up clock to run functions at a schedule (seconds)
+		Clock.schedule_interval(self.displayQueue, 1)
+		#build GUI from kv
+		self.root = Builder.load_file('gui/gui.kv')
+		return self.root
+		
+	def buttonHandler(self, func):
+		if(func == 'ac'):
+			print('Arm Camera Selected')
+			
+		if(func == 'dc'):
+			print('Drive Camera Selected')
+		
+		if(func == 'none'):
+			print('Info: Button has no function')
+			
+	#test to display an amt of Queue items on label
+	def displayQueue(self, amt):
+		if not self.mailbox.empty():
+			self.ltb2.l_text = str(self.mailbox.get())
+		else:
+			print("no data in queue")
+	
+	def on_start(self):
+		##Set up Queue
+		self.mailbox = Queue()
+			
+		##Set up the threads
+		# make each top-level thread
+		self.commThread = communicationThread()
+		self.inputThread = inputThread()
+		self.navThread = navigationThread()
+		self.panelThread = panelThread()
+		
+		# configure threads
+		self.commThread.sendPort = 8001
+		self.commThread.receivePort = 8000
+		self.commThread.sendInterval = 0.25
+		self.commThread.inputThread = self.inputThread
+		self.commThread.navThread = self.navThread
+		self.commThread.panelThread = self.panelThread
+		self.commThread.guiThread = self
+		self.inputThread.commThread = self.commThread
+		
+		##Load the other threads
+		print("starting")
+		self.startThreads()
+	
+	def startThreads(self):
+		self.commThread.start()
+		self.inputThread.start()
+		self.navThread.start()
+		self.panelThread.start()
 
+	def stopThreads(self):
+		self.inputThread.stop()
+		self.commThread.stop()
+		self.navThread.stop()
+		self.panelThread.stop()
+		
+	def on_stop(self):
+		print('exiting')
+		#Unload all threads
+		self.stopThreads()
 
-# make each top-level thread
-commThread = communicationThread()
-inputThread = inputThread()
-navThread = navigationThread()
-panelThread = panelThread()
-guiThread = guiThread()
-
-# configure threads
-commThread.sendPort = 8001
-commThread.receivePort = 8000
-commThread.sendInterval = 0.25
-commThread.inputThread = inputThread
-commThread.navThread = navThread
-commThread.panelThread = panelThread
-commThread.guiThread = guiThread
-
-inputThread.commThread = commThread
-
-# test execution
-print("starting")
-startThreads()
-print("running")
-time.sleep(2)
-
-# test code goes here
-
-time.sleep(20)
-print("stopping")
-stopThreads()
-print("done")
-
+try:
+	KivyGuiApp().run()
+except:
+	KivyGuiApp.stopThreads()
