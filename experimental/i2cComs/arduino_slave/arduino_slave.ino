674889b24d307f01d5cde7a1b3a1cda99d0fcf43
vpeng <vpeng@usst.ca>
2015-01-07 21:50:12 +0000

    Added some information on comms using i2c

---
+++ experimental/i2cComs/arduino_slave/arduino_slave.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+
+#define LED_PIN 13
+
+byte slave_address = 7;
+byte CMD_ON = 0x00;
+byte CMD_OFF = 0x01;
+
+void setup() {
+  // Start I2C Bus as Slave
+  Wire.begin(slave_address);
+  Wire.onReceive(receiveEvent);
+  
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);  
+
+}
+
+void loop() {
+
+}
+
+void receiveEvent(int howMany) {
+  byte cmd = Wire.read();
+  if (cmd == CMD_ON){
+    digitalWrite(LED_PIN, HIGH);
+  }else if(cmd == CMD_OFF){
+    digitalWrite(LED_PIN, LOW);
+  }
+}
+
