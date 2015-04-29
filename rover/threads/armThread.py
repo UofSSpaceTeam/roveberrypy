02c5fe366ad5cab9a7c7fcdeb23c6a0594bc7a42
usstworkstation <usstworkstation@05bd7b0d-e772-0f9f-b542-7bb087f2e3bb>
2015-04-29 17:49:07 +0000

    armThread expansion

--- rover/threads/armThread.py
+++ rover/threads/armThread.py
@@ -11,15 +11,16 @@ import struct
 # matching structures from arduino
 class CommandType:
 	stop = 0x00
-	setSpeed = 0x01
-	setMotor = 0x02
-
+	setPos = 0x01
+	nudge = 0x02
 class Command:
 	def __init__(self):
 		self.header = 0xF7
 		self.type = 0x00
 		self.d1 = 0x0000
-		#self.d2 = 0x0000
+		self.d2 = 0x0000
+		self.d3 = 0x0000
+		self.d4 = 0x0000
 		self.csum = 0x00
 		self.trailer = 0xF8
 
@@ -39,7 +40,9 @@ class armThread(threading.Thread):
 	def run(self):
 		command = Command()
 		baseSpeed = None
-		throttle = 0.3
+		L1 = None
+		L2 = None
+		L3 = None
 		print "arm thread started"
 		while True:
 			while not self.mailbox.empty():
@@ -50,22 +53,41 @@ class armThread(threading.Thread):
 					if "c1t" in data:
 						baseSpeed = int(data["c1t"] * 255) # -255 to 255
 						print baseSpeed
+					if "c1j1y" in data:
+						L1 = int(data["c1j1y"] * 255)
+						print L1
 			
+			baseSpeed = 128
+			L1 = 101
+			L2 = 56
+			L3 = 98
 			# send on complete input update
-			if baseSpeed is not None:
-				command.type = CommandType.setSpeed
-				command.d1 = int(baseSpeed * throttle)
+			if baseSpeed is not None and L1 is not None and L2 is not None:
+				command.type = CommandType.setPos
+				command.d1 = int(baseSpeed)
+				command.d2 = int(L1)
+				command.d3 = int(L2)
+				command.d4 = int(L3)
 				baseSpeed = None
+				L1 = None
+				L2 = None
+				L3 = None
 				self.sendCommand(command)
 			time.sleep(0.01)
 
 	def sendCommand(self, command):
-		command.csum = (command.type + command.d1) % 256
+		command.csum = (command.type + command.d1 + command.d2 + command.d3 + command.d4) % 256
 		try:
 			i2c.write_byte(address, command.header)
 			i2c.write_byte(address, command.type)
 			i2c.write_byte(address, command.d1 & 0xFF)
 			i2c.write_byte(address, command.d1 >> 8)
+			i2c.write_byte(address, command.d2 & 0xFF)
+			i2c.write_byte(address, command.d2 >> 8)
+			i2c.write_byte(address, command.d3 & 0xFF)
+			i2c.write_byte(address, command.d3 >> 8)
+			i2c.write_byte(address, command.d4 & 0xFF)
+			i2c.write_byte(address, command.d4 >> 8)
 			i2c.write_byte(address, command.csum)
 			i2c.write_byte(address, command.trailer)
 		except IOError:
