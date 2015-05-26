27d6c2e7b00178b22deb6e614368ee6500763fb6
jordan.kubica <jordan.kubica@gmail.com>
2015-05-26 02:25:15 +0000

    new motor control code for drill rig

--- rover/Drill Module Slave/drillModule/drillModule.ino
+++ rover/Drill Module Slave/drillModule/drillModule.ino
@@ -33,15 +33,20 @@ typedef struct
 */
 
 // pin connetions for motor
-const byte m_pwm[] = {10, 11};
 const byte l_pwr[] = {4, 6, 5};
+byte drillPin = 10;
+byte elevPin = 11;
 
 // arduino address on bus
 const byte i2c_address = 0x09;
 
 // servo stuff for motors
 Servo drillMotor;
+short drillSpeed = 0;
+short desiredDrillSpeed = 0;
 Servo elevMotor;
+short elevSpeed = 0;
+short desiredElevSpeed = 0;
 
 // control data
 volatile short m_cmd[] = {0, 0}; // commanded speeds
@@ -58,21 +63,11 @@ volatile byte cmd_count = 0;
 volatile bool new_cmd = false;
 
 
-// function prototypes
-
-// Interrupt handler for receiving a byte via I2C
 void receiveEvent(int count);
-
-// function to actually process command
 void processCommand();
-
-/*
-	sets a single motor to the specified direction and power level.
-	index: which motor, [0, 1]
-	value: speed and direction, [-255, 255]
-*/
-void setMotor(byte index, short value);
-
+void updateDrill();
+void updateElev();
+void stopAll();
 /*
 	Turns on a single laser
 	index: which laser, [0, 1, 2]
@@ -80,58 +75,129 @@ void setMotor(byte index, short value);
 */
 void setLaser(byte index, short value);
 
-// stops all motors and lasers.
-void stopAll();
-
-
-// functions
-
-void setup() {
-	Serial.begin(9600); // debug
+void setup()
+{
+	Serial.begin(9600);
 	Wire.begin(i2c_address);
 	Wire.onReceive(receiveEvent);
-	Wire.onRequest(requestEvent);
-		
-	drillMotor.attach(10);
-	elevMotor.attach(11);
-        stopAll();
-
-        pinMode(5, OUTPUT);
-        pinMode(4, OUTPUT);
-        pinMode(6, OUTPUT);
-        	
-	// clear cmd struct
-	for(int i = 0; i < sizeof(command); i++)
-		cmd_ptr[i] = 0x00;
 	
-	// arm timeout
+	pinMode(drillPin, OUTPUT);
+	pinMode(elevPin, OUTPUT);
+	for(int i = 0; i < 3; i++)
+		pinMode(l_pwr[i], OUTPUT);
+	
+	drillMotor.attach(drillPin);
+	elevMotor.attach(elevPin);
+	stopAll();
+		
 	timeout = millis();
-	prevTime = millis();
+	prevTime = timeout;
 }
 
 void loop()
 {
 	if(new_cmd)
-	{	 
-		//Serial.println("New Command");
 		processCommand();
-		new_cmd = false;
+	
+	updateDrill();
+	updateElev();
+	
+	if(millis() - timeout > TIMEOUT)
+	{
+		Serial.println("TO");
+		stopAll();
+		timeout = millis();
 	}
-	for(int i = 0; i < 2; i++)
-	{	
-		setMotor(i, m_cmd[i]);
-		
-		if(millis() - timeout > TIMEOUT)
-		{
-			Serial.println("TO");
-			stopAll();
-			timeout = millis();
-		}
-	}
-	for(int i = 0; i < 3; i++) {
+	for(int i = 0; i < 3; i++)
 		setLaser(i, l_cmd[i]);
+	delay(50);
+}
+
+void processCommand()
+{
+	switch(cmd.type)
+	{
+		case STOP:
+		stopAll();
+		break;
+		
+		case SET_SPEED:
+		desiredDrillSpeed = constrain(cmd.d1, -255, 255);
+		desiredElevSpeed = constrain(cmd.d2, -255, 255);
+		timeout = millis();
+		break;
+		
+		case SET_LASER:
+		Serial.println("laser");
+		if(cmd.d1 > 3 || cmd.d1 < 0)
+			break;
+		if(cmd.d2 > 1 || cmd.d1 < 0)
+			break;
+		l_cmd[cmd.d1 - 1] = cmd.d2;
+		timeout = millis();
+		Serial.print(cmd.d1);
+		Serial.print(", ");
+		Serial.println(cmd.d2);
+		break;
 	}
-        delay(50);
+	new_cmd = false;
+}
+
+void updateDrill()
+{
+	if(desiredDrillSpeed == 0)
+	{
+		drillMotor.writeMicroseconds(1500);
+		return;
+	}
+	else if(abs(desiredDrillSpeed - drillSpeed) < 5)
+		drillSpeed = desiredDrillSpeed;
+	else if(desiredDrillSpeed > drillSpeed)
+		drillSpeed += 5;
+	else
+		drillSpeed -= 5;
+	drillMotor.writeMicroseconds(map(drillSpeed, -255, 255, 900, 2100));
+}
+	
+void updateElev()
+{
+	if(desiredElevSpeed == 0)
+	{
+		elevMotor.writeMicroseconds(1500);
+		return;
+	}
+	else if(abs(desiredElevSpeed - elevSpeed) < 5)
+		elevSpeed = desiredElevSpeed;
+	else if(desiredElevSpeed > elevSpeed)
+		elevSpeed += 5;
+	else
+		elevSpeed -= 5;
+	elevMotor.writeMicroseconds(map(elevSpeed, -255, 255, 900, 2100));
+}
+
+void setLaser(byte index, short value)
+{
+  //Serial.print(l_pwr[index]);
+  //Serial.println(value);
+  if(index == 2){
+	//if(value) Serial.println("P Laser On");
+	//else Serial.println("P Laser Off");  
+   digitalWrite(l_pwr[index], !value);
+  }
+  else{
+	digitalWrite(l_pwr[index], value);
+	}
+}
+
+void stopAll()
+{
+	Serial.println("stopped");
+	drillSpeed = 0;
+	desiredDrillSpeed = 0;
+	elevSpeed = 0;
+	desiredElevSpeed = 0;
+	drillMotor.writeMicroseconds(1500);
+	elevMotor.writeMicroseconds(1500);
 }
 
 void receiveEvent(int count)
@@ -177,93 +243,3 @@ void receiveEvent(int count)
 	}
 }
 
-void processCommand()
-{
-	//Serial.println("got command");
-		//Serial.println(cmd.type);
-		switch(cmd.type)
-	{
-		case STOP:
-		stopAll();
-		break;
-		
-		case SET_SPEED:
-               Serial.println("speeds");
-		if(cmd.d1 > 255 || cmd.d1 < -255)
-			break;
-		if(cmd.d2 > 255 || cmd.d2 < -255)
-			break;
-		m_cmd[0] = cmd.d1;
-		m_cmd[1] = cmd.d2;
-			timeout = millis();
-                Serial.print(cmd.d1);
-                Serial.print(",");
-                Serial.println(cmd.d2);
-		break;
-		
-		case SET_LASER:
-                Serial.println("laser");
-		if(cmd.d1 > 3 || cmd.d1 < 0)
-			break;
-		if(cmd.d2 > 1 || cmd.d1 < 0)
-			break;
-		l_cmd[cmd.d1 - 1] = cmd.d2;
-			timeout = millis();
-                Serial.print(cmd.d1);
-                Serial.print(",");
-                Serial.println(cmd.d2);
-		break;
-		
-	}
-}
-
-void requestEvent()
-{
-	return;
-}
-
-void setMotor(byte index, short value)
-{
-		Serial.print("SetSpeeds: ");
-                
-                int pwm = map(value, -255, 255, 800, 2200);
-		Serial.println(pwm);
-                if(index == 0) {
-		  if(value == 0) {  
-			drillMotor.writeMicroseconds(1500);
-		  }
-		 else{
-			drillMotor.writeMicroseconds(pwm);
-		 }
-		}
-		else {
-			if(value == 0) {
-				elevMotor.writeMicroseconds(1500);
-			}
-		else {
-			elevMotor.writeMicroseconds(pwm);
-		}
-	}
-}
-
-void setLaser(byte index, short value)
-{
-  //Serial.print(l_pwr[index]);
-  //Serial.println(value);
-  if(index == 2){
-    //if(value) Serial.println("P Laser On");
-    //else Serial.println("P Laser Off");  
-   digitalWrite(l_pwr[index], !value);
-  }
-  else{
-    digitalWrite(l_pwr[index], value);
-    }
-}
-
-void stopAll()
-{
-	Serial.println("stopped");
-		drillMotor.writeMicroseconds(1500);
-		elevMotor.writeMicroseconds(1500);
-
-}
