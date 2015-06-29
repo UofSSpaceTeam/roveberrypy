717b4d9be3476780e6d28ee46689aa8d7f5237d5
DylanM04 <dylanmilne04@gmail.com>
2015-06-28 21:55:34 -0600

    added c# guitest to the base station.  currently is simple gui for xbox controller.  Still need to add support for Json and Oculus rift

---
+++ base/usstguiTest/README.txt
@@ -0,0 +1,4 @@
+To run this you will need to have visual studio installed on your computer.
+Then to get the sharpdx libraries to work you will need to go to your solution explorer and add references.
+choose browse and find where your github repository is.  Then take the .dlls from directx11-net40.
+The currently needed sharpdx dlls are Xinput and directInput.
---
+++ base/usstguiTest/usstgui/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using SharpDX.XInput;
+using System;
+using System.Threading;
+
+namespace usstgui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Gamepad xbox;
+        Controller controller;
+        double scale = 1;
+        private bool running = true;
+        private int xinversion = 1;
+        private int yinversion = 1;
+        public MainWindow()
+        {
+            controller = new SharpDX.XInput.Controller(SharpDX.XInput.UserIndex.One);
+            InitializeComponent();
+            Thread t = new Thread(new ThreadStart(XboxController));
+            t.Start();
+
+            //this.OnClosing += Cleanup;
+        }
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            Cleanup();
+            base.OnClosing(e);
+        }
+
+        protected void Cleanup()
+        {
+            running = false;
+        }
+
+        private void XboxController()
+        {
+            double x;
+            double y;
+            try
+            {
+                while(running)
+                {
+                    xbox = controller.GetState().Gamepad;
+                    y = (-2 + (xbox.LeftThumbY - (-32768)) * (2 + 2) / (32767 + 32768))*scale*yinversion;
+                    x = (-2 + (xbox.LeftThumbX - (-32768)) * (2 + 2) / (32767 + 32768))*scale*xinversion;
+                    Console.WriteLine("Y = " + y);
+                    Console.WriteLine("X = " +x);
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+            }
+        }
+        private void InvertX_Checked(object sender, RoutedEventArgs e)
+        {
+            xinversion = xinversion * (-1);
+
+        }
+
+        private void InvertY_Checked(object sender, RoutedEventArgs e)
+        {
+            yinversion = yinversion * (-1);
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            scale = scaler.Value;
+        }
+    }
+}
