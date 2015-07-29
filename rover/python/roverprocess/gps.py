7e2c8e7dcbefd5cc432fe44d3f3aaab0b90bd4f1
Carl Hofmeister <hofmeister.carl@gmail.com>
2015-07-28 22:56:43 -0600

    GPS position reading moved to callable funtion. Tested with real data.

--- rover/python/roverprocess/gps.py
+++ rover/python/roverprocess/gps.py
@@ -2,7 +2,6 @@ from roverprocess import RoverProcess
 from sbp.client.drivers.pyserial_driver import PySerialDriver
 from sbp.client.handler import Handler
 from sbp.navigation import SBP_MSG_POS_LLH, MsgPosLLH
-from sbp.system import SBP_MSG_HEARTBEAT, MsgHeartbeat
 import time
 
 class GPS(RoverProcess):
@@ -13,37 +12,42 @@ class GPS(RoverProcess):
           import argparse
           parser = argparse.ArgumentParser(description="Swift Navigation SBP Example.")
           parser.add_argument("-p", "--port",
-                      default=['/dev/ttyUSB0'], nargs=1,
+                      default=['/dev/ttyUSB1'], nargs=1,
                       help="specify the serial port to use.")
           args = parser.parse_args()
 
           # Open a connection to Piksi using the default baud rate (1Mbaud)
           self.driver = PySerialDriver(args.port[0], baud=1000000)
           self.handler = Handler(self.driver.read, self.driver.write, verbose=True)
-          self.handler.add_callback(self.posLLH_callback, msg_type=SBP_MSG_POS_LLH)
+          #self.handler.add_callback(self.pos_callback, msg_type=SBP_MSG_POS_LLH)
           self.handler.start()
 
 	def loop(self):
                 # print self.driver.read(4)
+                self.getPos()
 		time.sleep(1)
 
 	def messageTrigger(self, message):
 		RoverProcess.messageTrigger(self, message)
 		if "gpsMsg" in message:
 			print "got: " + str(message["gpsMsg"])
+                        self.getPos()
 
 	def cleanup(self):
-		RoverProcess.cleanup(self)
                 self.handler.stop()
+		RoverProcess.cleanup(self)
 
 	# additional functions go here
 
-        def posLLH_callback(self, msg):
-            # This is called every time we receive a POS_LLH message
-            p = MsgPosLLH(msg)
+        # def pos_callback(self, msg):
+        #     p = MsgPosLLH(msg)
+        #     print "%.4f,%.4f,%.4f,%.i" % \
+        #             (p.lat, p.lon, p.height, p.flags)
 
-            # Print out the latitude, longtitude, and height
-            print "%.4f,%.4f,%.4f" % \
-                    (p.lat, p.lon, p.height)
-
+        def getPos(self):
+            #wait for a POS_LLH mesg (1 second timeout)
+            p = MsgPosLLH(self.handler.wait(msg_type=SBP_MSG_POS_LLH, timeout=5.0))
+            if not p == None:
+                print "%.4f,%.4f,%.4f,%.i" % \
+                        (p.lat, p.lon, p.height, p.flags)
 
