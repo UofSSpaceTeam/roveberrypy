c7a2800880af869d51d94a1ccf43d282634fe914
Austin Shirley <ausshir@gmail.com>
2015-08-12 12:53:01 -0600

    Made maps actually show
    Whoops. :)

--- base/usstgui/Windows/NavWindow.xaml.cs
+++ base/usstgui/Windows/NavWindow.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
+using System.Net.NetworkInformation;
 
 namespace usstgui
 {
@@ -22,6 +26,39 @@ namespace usstgui
         public NavWindow()
         {
             InitializeComponent();
+
+            if (!PingNetwork("pingtest.com"))
+            {
+                MainMap.Manager.Mode = AccessMode.CacheOnly;
+                MessageBox.Show("No internet connection available, going to CacheOnly mode.", "GMap.NET - Demo.WindowsPresentation", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            MainMap.MapProvider = GMapProviders.OpenStreetMap;
+            MainMap.Position = new PointLatLng(52.13225, -106.62914);
+        }
+
+        //Kinda dumb helper to check if connected.. could be better?
+        public static bool PingNetwork(string hostNameOrAddress)
+        {
+            bool pingStatus = false;
+
+            using (Ping p = new Ping())
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+                int timeout = 4444; // 4s
+
+                try
+                {
+                    PingReply reply = p.Send(hostNameOrAddress, timeout, buffer);
+                    pingStatus = (reply.Status == IPStatus.Success);
+                }
+                catch (Exception)
+                {
+                    pingStatus = false;
+                }
+            }
+
+            return pingStatus;
         }
     }
 }
