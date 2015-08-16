be7869194ac7e4221245ca03eb4ad9a65b18ea9a
Jordan Kubica <jordan.kubica@gmail.com>
2015-08-16 16:31:22 -0600

    simplified drill operation

--- rover/python/roverprocess/drillprocess.py
+++ rover/python/roverprocess/drillprocess.py
@@ -22,50 +22,27 @@ class DrillProcess(RoverProcess):
 		self.i2c = smbus.SMBus(1)
 		self.i2cAddress = 0x09
 		self.i2cSem = args["sem"]
-		self.drillSpeed = 0.3
-		self.elevSpeed = 0.3
-		self.drillDir = 0
-		self.elevDir = 0
-		self.drillCw = False
-		self.drillCcw = False
-		self.drillDirChange = False
+		self.rotation = 0.0
+		self.translation = 0.0
 
 	def loop(self):
 		self.setDrill()
-		time.sleep(0.05)
+		time.sleep(0.2)
 
 	def messageTrigger(self, message):
 		RoverProcess.messageTrigger(self, message)
 		if "drillHeartbeat" in message:
 			self.setShared("drillHeartbeat", True)
-		if "DrillSpeed" in message:
-			self.drillSpeed = float(message["DrillSpeed"])
-		if "DrillFeed" in message:
-			self.elevSpeed = float(message["DrillFeed"])
-		if "DrillUp" in message:
-			self.elevDir = int(message["DrillUp"] == "True")
-		if "DrillDn" in message:
-			self.elevDir = -1*int(message["DrillDn"] == "True")
-		if "DrillCw" in message:
-			self.drillCw = (message["DrillCw"] == "True")
-		if "DrillCcw" in message:
-			self.drillCcw = (message["DrillCcw"] == "True")
+		if "drillRotation" in message:
+			self.rotation = float(message["drillRotation"])
+		if "drillTranslation" in message:
+			self.translation = float(message["drillTranslation"])
 
-	# additional functions go here
 	def setDrill(self):
 		command = Command()
 		command.type = CommandType.SetSpeed
-		
-		if(self.drillCw and not self.drillCcw):
-			self.drillDir = -1
-		elif(self.drillCcw and not self.drillCw):
-			self.drillDir = 1
-		else:
-			self.drillDir = 0
-				
-		
-		command.d1 = int(self.drillSpeed * self.drillDir * 255)
-		command.d2 = int(self.elevSpeed * self.elevDir * 255)
+		command.d1 = int(self.rotation * 255)
+		command.d2 = int(self.translation * 255)
 		self.sendCommand(command)
 
 	def sendCommand(self, command):
@@ -84,7 +61,5 @@ class DrillProcess(RoverProcess):
 			print("Arm thread got IOError")
 		self.i2cSem.release()
 
-
 	def cleanup(self):
 		RoverProcess.cleanup(self)
-		# your cleanup code here. e.g. stopAllMotors()
