bb1f9ff292070f4a49d54cba9b338af1cebf9ef9
LIAM BINDLE <lrbindle@gmail.com>
2015-09-03 16:07:01 +0200

    Added nice arm visualizations
    Liam broked it and Austin fixeded it. ;)

--- base/usstgui/usstgui/Windows/ArmStatus.xaml.cs
+++ base/usstgui/usstgui/Windows/ArmStatus.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Threading;
 using System.Diagnostics;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace usstgui
 {
@@ -13,6 +15,7 @@ namespace usstgui
         public ArmStatus()
         {
             InitializeComponent();
+            m_stepSize = 10;
         }
 
         private void updateArmLimits()
@@ -56,6 +59,16 @@ namespace usstgui
 
             ArmXStat.Value = xNorm;
             ArmYStat.Value = yNorm;
+
+            try
+            {
+                updateArmDrawing();
+            }
+            catch (Exception)
+            {
+                ArmDrawing.Children.Clear();                
+            }
+            
 
         }
 
@@ -128,6 +141,95 @@ namespace usstgui
         {
             Button pressed = (Button)sender;
             SharedState.set(pressed.Name, (e.ButtonState==MouseButtonState.Pressed).ToString());
+        }
+
+        private void updateArmDrawing()
+        {
+            double r = Convert.ToDouble(IK_XVal.Text)/1000.0;
+            double z = Convert.ToDouble(IK_YVal.Text)/1000.0;
+
+            double L1 = 0.33595;
+            double L2 = 0.36085;
+            double phi1 = Math.Acos(1.0/(2 * L1*L1 * (r*r + z*z)) * (L1 * L1 * L1 * z + L1 * z * (-L2 * L2 + r * r + z * z) 
+                            + (r * r + z * z) * Math.Sqrt(-(L1 * L1 * r * r * (L1 * L1 * L1 * L1 + (-L2 * L2 + r * r + z * z) 
+                            * (-L2 * L2 + r * r + z * z) - 2 * L1 * L1 * (L2 * L2 + r * r + z * z))) / ((r * r + z * z) * (r * r + z * z)))));
+            double phi2 = -Math.Acos(1.0/((-2 * L1 * L2 * (r*r + z*z))) * (L1*L1*L1 * z - L1 * z * (L2*L2 + r*r + z*z) 
+                            + (r*r + z*z) * Math.Sqrt(-(L1*L1 * r*r * (L1*L1*L1*L1 + (-L2*L2 + r*r + z*z)* (-L2 * L2 + r * r + z * z) 
+                            - 2 * L1*L1 * (L2*L2 + r*r + z*z))) / ((r*r + z*z)* (r * r + z * z)))));
+            ArmDrawing.Children.Clear();
+
+            // get coordinates in meters
+            double pivX = Math.Abs(L1 * Math.Sin(phi1)); 
+            double pivY = Math.Abs(L1 * Math.Cos(phi1));
+
+            const double ppm = 667.1899529; // pixels per meter
+
+            Line bar1 = new Line();
+            bar1.X1 = 110;
+            bar1.Y1 = 450;
+            bar1.X2 = 110 + pivX * ppm;
+            bar1.Y2 = 450 - pivY * ppm;
+            SolidColorBrush lineColor = new SolidColorBrush();
+            lineColor.Color = Colors.Black;
+            bar1.StrokeThickness = 3;
+            bar1.Stroke = lineColor;
+            Line bar2 = new Line();
+            bar2.X1 = bar1.X2;
+            bar2.Y1 = bar1.Y2;
+            bar2.X2 = 110 + r * ppm;
+            bar2.Y2 = 450 - z * ppm;
+            bar2.StrokeThickness = 3;
+            bar2.Stroke = lineColor;
+            ArmDrawing.Children.Add(bar1);
+            ArmDrawing.Children.Add(bar2);
+
+        }
+
+        double m_stepSize;
+        private void ArmDrawing_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key) {
+                // wasd controls for when using the figure
+                case Key.W:
+                    IK_YVal.Text = Convert.ToString(Convert.ToDouble(IK_YVal.Text) + m_stepSize);
+                    break;
+                case Key.S:
+                    IK_YVal.Text = Convert.ToString(Convert.ToDouble(IK_YVal.Text) - m_stepSize);
+                    break;
+                case Key.A:
+                    IK_XVal.Text = Convert.ToString(Convert.ToDouble(IK_XVal.Text) - m_stepSize);
+                    break;
+                case Key.D:
+                    IK_XVal.Text = Convert.ToString(Convert.ToDouble(IK_XVal.Text) + m_stepSize);
+                    break;
+                    // ik controls for use with cameras
+                case Key.I:
+                    IK_XVal.Text = Convert.ToString(Convert.ToDouble(IK_XVal.Text) + m_stepSize);
+                    break;
+                case Key.K:
+                    IK_XVal.Text = Convert.ToString(Convert.ToDouble(IK_XVal.Text) - m_stepSize);
+                    break;
+                case Key.PageUp:
+                    if(m_stepSize < 100 && Math.Abs(m_stepSize - 100) > 0.0001)
+                    {
+                        m_stepSize *= 10; 
+                    }
+                    break;
+                case Key.PageDown:
+                    if(Math.Abs(m_stepSize - 0.1) > 0.0001 && m_stepSize > 0.1)
+                    {
+                        m_stepSize /= 10;
+                    }
+                    break;
+            }
+            try
+            {
+                updateArmDrawing();
+            }
+            catch (Exception)
+            {
+                ArmDrawing.Children.Clear();
+            }
         }
     }
 }
--- base/usstgui/usstgui/Windows/LidarStatus.xaml.cs
+++ base/usstgui/usstgui/Windows/LidarStatus.xaml.cs
@@ -2,14 +2,141 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace usstgui
 {
     public partial class LidarStatus : Window
     {
+        const int PANEL_SIZE = 500;
+        double m_outer_radius; // m
+        enum ZOOM{IN, OUT };
+        const int PTS_PER_ROT = 100;
+        int m_positionIndex;
+        
+        class lidarDataPoint
+        {
+            public lidarDataPoint(double dist, double ang)
+            {
+                distance = dist;
+                angle = ang;
+            }
+            public void setDistance(double dist)
+            {
+                distance = dist;
+            }
+            public void setAngle(double ang)
+            {
+                angle = ang;
+            }
+            public double distance;
+            public double angle;
+        };
+        
+        lidarDataPoint[] m_data = new lidarDataPoint[PTS_PER_ROT];
+
+        public Ellipse LidarPoint(double radius, double theta)
+        {
+            const int PT_RADIUS = 4;
+            Ellipse ptObj = new Ellipse();
+            ptObj.Width = 2*PT_RADIUS;
+            ptObj.Height = 2*PT_RADIUS;
+            radius = getMetersToDigi(radius);
+            double x = PANEL_SIZE / 2 + (int)(radius * Math.Sin(theta) + 0.5) - PT_RADIUS;
+            double y = PANEL_SIZE / 2 - (int)( radius * Math.Cos(theta) + 0.5 ) - PT_RADIUS;
+            ptObj.Margin = new Thickness(x, y, 0.0, 0.0);
+            SolidColorBrush fillBrush = new SolidColorBrush();
+            fillBrush.Color = Color.FromArgb(255,255,0,0);
+            ptObj.Fill = fillBrush;
+            if (radius > 250)
+            {
+                ptObj.Visibility = Visibility.Hidden;
+            }
+            return ptObj;
+        }
+        private void setZoom(ZOOM direction)
+        {
+            if(m_outer_radius > 3 || direction == ZOOM.OUT) {
+                if(m_outer_radius == 5 && direction == ZOOM.IN) {
+                    m_outer_radius = 3;
+                } else if(m_outer_radius == 50 && direction == ZOOM.OUT) {
+                    // DO NOTHING
+                } else if(direction == ZOOM.IN) {
+                    m_outer_radius -= 5;
+                } else if(m_outer_radius == 3 && direction == ZOOM.OUT) {
+                    m_outer_radius = 5;
+                } else {
+                    m_outer_radius += 5;
+                }
+
+                Label1.Text = m_outer_radius.ToString() + "m";
+                Label2.Text = (m_outer_radius*0.8).ToString() + "m";
+                Label3.Text = (m_outer_radius*0.6).ToString() + "m";
+                Label4.Text = (m_outer_radius*0.4).ToString() + "m";
+                Label5.Text = (m_outer_radius*0.2).ToString() + "m";
+
+            } else {
+
+            }
+        }
+        private double getMetersToDigi(double m){
+            double val =  m * (PANEL_SIZE / 2 / m_outer_radius);
+            return val;
+        }
+
         public LidarStatus()
         {
             InitializeComponent();
+
+            m_outer_radius = 5;
+            setZoom(ZOOM.OUT);
+            inializeLidarArray();
+            updateDrawing();
+            m_positionIndex = 0;
+
+            for(int idx = 0; idx < 4; idx++)
+            {
+                pushLidarReading(2*idx, 0.866);
+            }
+            
+            
+
         }
-	}
+        public void inializeLidarArray()
+        {
+            for (int idx = 0; idx < PTS_PER_ROT; idx++)
+            {
+                m_data[idx] = new lidarDataPoint(10, 0);
+            }
+        }
+
+        public void pushLidarReading(double distance, double position)
+        {
+            m_data[m_positionIndex].distance = distance;
+            m_data[m_positionIndex].angle = position;
+            m_positionIndex = (m_positionIndex + 1) % PTS_PER_ROT;
+            updateDrawing();
+        }
+
+        public void updateDrawing()
+        {
+            drawSpace.Children.Clear();
+            for(int idx = 0; idx < PTS_PER_ROT; idx++)
+            {
+                drawSpace.Children.Add(LidarPoint(m_data[idx].distance, m_data[idx].angle));
+            }
+        }
+        private void btn_ZIN_Click(object sender, RoutedEventArgs e)
+        {
+            setZoom(ZOOM.IN);
+            updateDrawing();
+        }
+
+        private void btn_ZOUT_Click(object sender, RoutedEventArgs e)
+        {
+            setZoom(ZOOM.OUT);
+            updateDrawing();
+        }
+    }
 }
