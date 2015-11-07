ccabfb43f6c47bf268be9d20c84afccd1fa8b1f5
kylebonilla <kylebonilla2242@gmail.com>
2015-11-07 16:59:34 -0600

    added plywoodbot.py
    plywoodbot.py is an non thread version of plywoodbotprocess.py

--- plywoodbot/plywoodbotprocess.py
+++ plywoodbot/plywoodbotprocess.py
@@ -1,10 +1,18 @@
-import time 
-import smbus
+#****************************************
+#use plywoodbot.py instead
+#****************************************
+
+import os, sys
+sys.dont_write_bytecode = True
+import time
+#uncomment when on pi 
+#import smbus
 import pygame
+import multiprocessing
 
 class CommandType:
 		setMotors = 0x00
-		# todo: add the other commands and functionality
+		#todo: add the other commands and functionality
 
 class Command:
 	def __init__(self):
@@ -15,11 +23,38 @@ class Command:
 		self.csum = 0x00
 		self.trailer = 0xF8
 		
-class plywoodbotprocess:
+class plywoodbotProcess:
 
-	def __init__(self):
+	def __init__(self, **kwargs):
+		Process.__init__(self)
+		self.uplink = kwargs["uplink"]
+		self.downlink = kwargs["downlink"]
+		self._state = dict()
+		self._stateSem = BoundedSemaphore()
+		self._args = kwargs
+		self.load = True
+
+	def run(self):
+		receiver = RoverProcess.ReceiverThread(
+			self.downlink, self._state, self._stateSem, self)
+		receiver.start()
+		try:
+			self.setup(self._args)
+			while True:
+				self.loop()
+		except KeyboardInterrupt:
+			self.cleanup()
+			sys.exit(0)
+		except:
+			self.cleanup()
+			raise
+
+	#==============================================================
 	
-		self.i2c = smbus.SMBus(1)
+	def setup(self):
+	
+		#uncomment when on pi
+		#self.i2c = smbus.SMBus(1)
 		self.i2cAddress = 0x07
 		self.i2cSem = args["sem"]    
 		self.joyAxes = [0.0, 0.0]
@@ -29,12 +64,8 @@ class plywoodbotprocess:
 			self.driveController = pygame.joystick.Joystick(0)
 			self.driveController.init()
 		else:
-			print("No controller")
+			print("No controller") 
 	
-	def run(self):
-		while True:
-			self.loop; 
-		
 		
 	def loop(self):
 		pygame.event.pump()
@@ -67,22 +98,26 @@ class plywoodbotprocess:
 		command = Command()
 		command.type = CommandType.setMotors
 		command.d1 = int(speeds[0] * 255) # left
+		print "left input", command.d1
 		command.d2 = int(speeds[1] * 255) # right
+		print "right input", command.d2
 		
 		self.sendCommand(command)
 		
 	def sendCommand(self, command):
 		command.csum = (command.type + command.d1 + command.d2) % 256
 		try:
-			self.i2cSem.acquire(block=True, timeout=None)
-			self.i2c.write_byte(self.i2cAddress, command.header)
-			self.i2c.write_byte(self.i2cAddress, command.type)
-			self.i2c.write_byte(self.i2cAddress, command.d1 & 0xFF)
-			self.i2c.write_byte(self.i2cAddress, command.d1 >> 8)
-			self.i2c.write_byte(self.i2cAddress, command.d2 & 0xFF)
-			self.i2c.write_byte(self.i2cAddress, command.d2 >> 8)
-			self.i2c.write_byte(self.i2cAddress, command.csum)
-			self.i2c.write_byte(self.i2cAddress, command.trailer)
+			print("sending command to motors")
+			#uncomment when on pi
+			#self.i2cSem.acquire(block=True, timeout=None)
+			#self.i2c.write_byte(self.i2cAddress, command.header)
+			#self.i2c.write_byte(self.i2cAddress, command.type)
+			#self.i2c.write_byte(self.i2cAddress, command.d1 & 0xFF)
+			#self.i2c.write_byte(self.i2cAddress, command.d1 >> 8)
+			#self.i2c.write_byte(self.i2cAddress, command.d2 & 0xFF)
+			#self.i2c.write_byte(self.i2cAddress, command.d2 >> 8)
+			#self.i2c.write_byte(self.i2cAddress, command.csum)
+			#self.i2c.write_byte(self.i2cAddress, command.trailer)
 		except IOError:
 			print("Drive thread got IOError")
 		self.i2cSem.release()
@@ -95,3 +130,35 @@ class plywoodbotprocess:
 	def cleanup(self):
 		RoverProcess.cleanup(self)
 		# your cleanup code here. e.g. stopAllMotors()
+	
+	
+if(os.name == "nt"):
+	modulesList = ["plywoodbotProcess"]
+else:
+	modulesList = ["plywoodbotProcess"]
+	
+if __name__ == "__main__":
+
+	processes = []
+	i2cSem = multiprocessing.Semaphore(1)
+	print "\nBUILD: Registering process subsribers...\n"
+	
+	if "plywoodbotProcess" in modulesList:
+		process = plywoodbotProcess(
+			downlink = system.getDownlink(), uplink = system.getUplink())
+		processes.append(process)
+
+
+	# start everything
+	print "\nSTARTING: " + str([type(p).__name__ for p in processes]) + "\n"
+	for process in processes:
+		process.start()
+
+	# wait until ctrl-C or error
+	try:
+		while True:
+			time.sleep(60)
+	except KeyboardInterrupt:
+		print("\nSTOP: " + str([type(p).__name__ for p in processes]) + "\n")		
+	finally:
+		system.terminateState()
