3894d015d60eade0f7c6d5eafa1c2ec16dfb2c5f
Austin Shirley <ausshir@gmail.com>
2015-11-23 15:29:48 -0600

    Intermediate changes before importing WebUI code

--- coreRover/main.py
+++ coreRover/main.py
@@ -8,7 +8,7 @@ modulesList = []
 
 # Check if on windows - if so do not run rover hardware specific code!
 if(os.name == "nt"):
-	modulesList = ["JsonServer", "Example"]
+	modulesList = ["JsonServer", "Example", "WebServer"]
 else:
 	modulesList = ["JsonServer", "I2C"]
 
@@ -29,31 +29,31 @@ if __name__ == "__main__":
 	i2cSem = multiprocessing.Semaphore(1)
 	print "\nBUILD: Registering process subsribers...\n"
 
-	if "JsonServer" in modulesList: 
+	if "JsonServer" in modulesList:
 		process = JsonServer(
 			downlink = system.getDownlink(), uplink = system.getUplink(),
 			local = localPort, remote = remotePort, sendPeriod = 0.1)
 		system.addObserver("exampleTime", process.downlink)
 		processes.append(process)
-		
+
 	if "Example" in modulesList:
 		process = ExampleProcess(
 			downlink = system.getDownlink(), uplink = system.getUplink())
 		system.addObserver("exampleTime", process.downlink)
 		processes.append(process)
-		
+
 	if "WebServer" in modulesList:
 		process = WebserverProcess(
 			downlink = system.getDownlink(), uplink = system.getUplink())
 		processes.append(process)
 
-	
+
 	if "I2C" in modulesList:
 		process = I2cExampleProcess(
 			downlink = system.getDownlink(), uplink = system.getUplink(),
 			sem = i2cSem)
 		processes.append(process)
-	
+
 	# start everything
 	print "\nSTARTING: " + str([type(p).__name__ for p in processes]) + "\n"
 	for process in processes:
@@ -64,6 +64,6 @@ if __name__ == "__main__":
 		while True:
 			time.sleep(60)
 	except KeyboardInterrupt:
-		print("\nSTOP: " + str([type(p).__name__ for p in processes]) + "\n")		
+		print("\nSTOP: " + str([type(p).__name__ for p in processes]) + "\n")
 	finally:
 		system.terminateState()
