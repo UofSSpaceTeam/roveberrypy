8d0fc56ad13473352c83c589f20e9a88fa924dcf
Austin Shirley <ausshir@gmail.com>
2016-01-09 17:52:51 -0600

    Added some SBP over UDP stuff for navigation!
    Based off of
    http://docs.swiftnav.com/wiki/Alternate_Radio_Setup_(Send_SBP_over_UDP)

--- coreRover/roverprocess/Navprocess.py
+++ coreRover/roverprocess/Navprocess.py
@@ -3,35 +3,52 @@ from RoverProcess import RoverProcess
 
 # your imports go here. For example:
 import time
+import socket
+import serial
 
 class NavProcess(RoverProcess):
-	
+
 	def setup(self, args):
-		# your setup code here
-		self.run = False; 
-		
+		# Navigation status
+		self.run = False;
+
+		# UDP/Serial Link to SwiftNav Piksi Base software
+		# From https://github.com/swift-nav/piksi_tools/blob/master/piksi_tools/ardupilot/udp_receive.py#L16
+		self.piksiAddr = "127.0.0.1"
+		self.piksiPort = 13320
+		self.piksiSerial = "/dev/ttyUSB0"
+		self.piksiBaud = 1000000
+
+		self.serLink = serial.Serial(self.piksiSerial, self.piksiBaud)
+		self.sockLink = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.sockLink.bind(self.piksiAddr, self.piksiPort)
 
 	def loop(self):
-		# your looping code here
-	
-		time.sleep(1)
+		# Getting UDP/Serial Link
+		data, addr = self.sockLink.recvfrom(1024)
+		if data:
+			self.serLink.write(data)
+
+		time.sleep(0.01)
 
 	def messageTrigger(self, message):
 		RoverProcess.messageTrigger(self, message)
 		# your message handling here. for example:
 		#if "exampleKey" in message:
 		#	print "got: " + str(message["exampleKey"])
-	
-	
+
+
 	def cleanup(self):
 		RoverProcess.cleanup(self)
-		# your cleanup code here. e.g. stopAllMotors()
+		if self.serLink.isopen():
+			self.serLink.close()
+
 
 	#-----------------------------------------------------------
-	#navigation algorithm 
-		
-		
-		
+	#navigation algorithm
+
+
+
 	#-------------------------------------------------------------
 	#functions to interact with the rest of the system
 
@@ -40,7 +57,7 @@ class NavProcess(RoverProcess):
 	#needs to be called before any other nav function can be ran
 	def startNav(self):
 		global run = True
-		
+
 
 
 	#sets the motors to the giving values
@@ -50,7 +67,7 @@ class NavProcess(RoverProcess):
 	def move(self, left, right):
 		if (run):
 			#add code to map values between 0 to 1
-			
+
 			#send message to left side motors
 			self.setShared("inputOneLeftY", str(left))
 			#send message to right side motors
@@ -60,10 +77,6 @@ class NavProcess(RoverProcess):
 
 
 	#ends software control for the rover
-	#start Nav has to be called again 
+	#start Nav has to be called again
 	def endNav(self):
 		global run = False
-		
-
-
- 
