ec8bba288c019ea73d68853a0509a715e31c2aab
Carl Hofmeister <hofmeister.carl@gmail.com>
2016-05-24 22:00:30 -0600

    Got arm thread accepting xbox controller input.

--- coreRover/roverprocess/ArmProcess.py
+++ coreRover/roverprocess/ArmProcess.py
@@ -12,16 +12,16 @@ class Command:
     def __init__(self):
         self.type = 0x00
         self.position = [0,0,0]
-        self.velocity = [0,0,0,0,0,0]
+        self.duty_cycle = [0,0,0,0,0,0]
 
     def csum(self):
-        return (sum(self.position) + sum(self.velocity)) % 256
+        return (sum(self.position) + sum(self.duty_cycle)) % 256
 
 class ArmProcess(RoverProcess):
 
     def getSubscribed(self):
         return {
-                "self" : [],
+                "self" : ["axes"],
                 "json" : [],
                 "can" : [],
                 "web" : []
@@ -32,7 +32,9 @@ class ArmProcess(RoverProcess):
         self.address = 0x07
         self.i2cSem = args["sem"]
         self.update = False
-        self.feedback = [0,0,0,0,0,0]
+        self.feedback = [0,0]
+        self.dutys = [0,0,0,0,0,0]
+        self.positions = [0,0,0]
 
     def loop(self):
         val = False
@@ -41,31 +43,34 @@ class ArmProcess(RoverProcess):
             c = Command();
             c.type = CommandType.INVERSE_KIN
             c.position = [300,2,-512]
-            c.velocity = [-20,50,40,-100,20,10]
-            #self.sendCommand(c)
-            temp_feedback = self.requestPosition()
-            if not None in temp_feedback:
-                self.feedback = temp_feedback
-            else:
-                print("got invalid data");
-            time.sleep(3)
+            c.duty_cycle = self.dutys
+            c.position = self.positions
+            self.sendCommand(c)
+            #temp_feedback = self.requestPosition()
+            #if not None in temp_feedback:
+            #    self.feedback = temp_feedback
+            #else:
+            #    print("got invalid data");
+            time.sleep(0.1)
 
     def messageTrigger(self, message):
         RoverProcess.messageTrigger(self, message)
+        if "axes" in message:
+            self.dutys = [int(float(x)*127) for x in message["axes"]]
 
     def sendCommand(self, command):
         #break command.position into 6 8-bit values, lsb first
         buff = [0, 0, 0, 0, 0, 0]
-        print("commanding")
         for i in range(0,5,2):
             buff[i] = command.position[i/2] & 0x00FF
             buff[i+1] = (command.position[i/2] & 0xFF00) >> 8
 
+        #print(command.duty_cycle)
         try:
             self.i2cSem.acquire(block=True, timeout=None)
             print(command.csum())
             self.i2c.write_i2c_block_data(self.address, command.type,
-                    buff + command.velocity + [command.csum()])
+                    buff + command.duty_cycle + [command.csum()])
         except:
             print("Arm thread got an I2C error")
         self.i2cSem.release()
