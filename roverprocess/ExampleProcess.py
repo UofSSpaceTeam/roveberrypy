de795b2e4741a4194dc64ba4f6e7db9abaf92c86
Carl Hofmeister <hofmeister.carl@gmail.com>
2016-09-14 12:48:50 -0600

    Fixed import error after merging dev

--- roverprocess/ExampleProcess.py
+++ roverprocess/ExampleProcess.py
@@ -11,7 +11,7 @@
 # or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from RoverProcess import RoverProcess
+from .RoverProcess import RoverProcess
 
 # Any libraries you need can be imported here. You almost always need time!
 import time
