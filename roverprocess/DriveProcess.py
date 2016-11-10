02532d9716d8557c3ea7f6faa30913e8ce152933
wasee <wasee@LAPTOP-Q9V5TUT0>
2016-11-10 14:59:57 -0600

    Uncluttered DriveProcess.py from unneeded functions.

--- roverprocess/DriveProcess.py
+++ roverprocess/DriveProcess.py
@@ -13,58 +13,12 @@
 
 from .RoverProcess import RoverProcess
 
-# Any libraries you need can be imported here. You almost always need time!
-import time
-
 
 class DriveProcess(RoverProcess):
 
-	# See the tutorials on the wiki for things like basic threads and custom libraries.
-	# 	< TO DO: INSTERT LINK TO THAT WIKI AND WRITE IT >
-	# This is a tool to communicate with the main state manager at startup.
-	# 	It returns a dictionary of lists for all the incoming (self) and outgoing (server) messages
-	#	so that the StateManager knows who gets what message.
-	# Just put the name of the message in the relevant list.
+    # Subscribed to joystick1 and joystick2.
 	def getSubscribed(self):
 		return ["joystick1", "joystick2"]
-
-	# This is run once to set up anything you need.
-	# 	Hint: use the self object to store variables global to this process.
-	# 	You can also take any args you need from the main startup as a dictionary:
-	# 		self.something = args["something"]
-	# 	This can be handy for sharing semaphores with other processes!
-	def setup(self, args):
-		self.someVariable = 42
-
-	# This automatically loops forever.
-	#	It is best to put a time.sleep(x) at the end. This makes sure that it
-	#	will always run at the same time, and will give other processes time to run too!
-	# Use self.setShared() to send some variables to another process or server!
-	def loop(self):
-		self.publish("TestData", time.time())
-		time.sleep(1)
-
-	# This runs every time a new message comes in.
-	#	It is often handy to have an if statement for every type of message you expect
-	def messageTrigger(self, message):
-		RoverProcess.messageTrigger(self, message)
-
-		if "heartbeat" in message:
-			print("got: " + str(message["heartbeat"]))
-
-	# This runs once at the end when the program shuts down.
-	#	You can use this to do something like stop motors clean up open files
-	def cleanup(self):
-		# If you override this method, you must call RoverProcess.cleanup(self)
-		RoverProcess.cleanup(self)
-
-	# Whenever the "heartbeat" message is produced, the function "on_heartbeat"
-	# is called. This is an alternative to using large if/else structures in
-	# messageTrigger. In this function, message is the contents of the message,
-	# not the dictionary that contains multiple keys like in messageTrigger.
-	def on_heartbeat(self, message):
-		print("From callback got: " + str(messsage))
-
 
 	# Function that grabs the x and y axis values in message, then formats the data
 	#  and prints the result to stdout.
@@ -75,7 +29,7 @@ class DriveProcess(RoverProcess):
 		x_axis = (x_axis * 255)
 		y_axis = (y_axis * 255)
 		newMessage = [x_axis, y_axis]
-		print(newMessage)
+		#print(newMessage)
 		self.publish("wheel1", newMessage)
 		self.publish("wheel2", newMessage)
 		self.publish("wheel3", newMessage)
@@ -90,7 +44,7 @@ class DriveProcess(RoverProcess):
 		x_axis = (x_axis * 255)
 		y_axis = (y_axis * 255)
 		newMessage = [x_axis, y_axis]
-		print(newMessage)
+		#print(newMessage)
 		self.publish("wheel4", newMessage)
 		self.publish("wheel5", newMessage)
 		self.publish("wheel6", newMessage)
