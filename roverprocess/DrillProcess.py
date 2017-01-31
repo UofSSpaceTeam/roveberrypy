12c2b81e5367f8f5078efa4556a90053db30f5f9
Thomas-Hu <hu.hut.thomas@gmail.com>
2017-01-31 17:20:04 -0600

    Created DrillProcess

---
+++ roverprocess/DrillProcess.py
@@ -0,0 +1,74 @@
+# Copyright 2016 University of Saskatchewan Space Design Team Licensed under the
+# Educational Community License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+#
+# https://opensource.org/licenses/ecl2.php
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+from .RoverProcess import RoverProcess
+
+
+class DrillProcess(RoverProcess):
+
+    # Subscribed to joystick1 and joystick2.
+	def getSubscribed(self):
+		return ["button1", "button2"]
+
+	def setup(self, args):
+		for key in ["button1", "button2"]:
+			self.subscribe(key)
+
+	# Function that grabs the x and y axis values in message, then formats the data
+	#  and prints the result to stdout.
+	# Returns the newly formated x and y axis values in a new list
+	def on_button1(self, message):
+		"""
+		y_axis = message[1]
+		y_axis = (y_axis * 40000/2)
+		if y_axis > 11000 or y_axis < -11000:
+			newMessage = y_axis
+		else:
+			newMessage = 0
+		"""
+		self.log(newMessage, "DEBUG")
+		self.publish("motor1", newMessage)
+		self.publish("motor2", newMessage)
+		self.publish("motor3", newMessage)
+
+
+	# Function that grabs the x and y axis values in message, then formats the data
+	#  and prints the result to stdout.
+	# Returns the newly formated x and y axis values in a new list
+	def on_button2(self, message):
+		"""
+		y_axis = message[2]
+		y_axis = (y_axis * 40000/2) # half power for testing
+		if y_axis > 11000 or y_axis < -11000:
+			newMessage = y_axis
+		else:
+			newMessage = 0
+		"""
+		
+		self.log(newMessage, "DEBUG")
+		self.publish("motor1", newMessage) #assuming that drill has 3 motors
+		self.publish("motor2", newMessage)
+		self.publish("motor3", newMessage)
+
+
+
+
+
+
+
+
+
+
+
+
+
