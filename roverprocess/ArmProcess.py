c94f8d397bb6e5579062a38f09d3f747623e7fea
chameleon07 <lcc746@mail.usask.ca>
2017-02-14 20:19:22 -0600

    Code for triggers

--- roverprocess/ArmProcess.py
+++ roverprocess/ArmProcess.py
@@ -12,38 +12,64 @@
 # permissions and limitations under the License.
 
 from .RoverProcess import RoverProcess
+import pyvesc
 
 # Any libraries you need can be imported here. You almost always need time!
 import time
 from multiprocessing.synchronize import BoundedSemaphore # BoundedSemaphore class used to block extra commands from conflicting with actual commands in operation.
 
+base_max_speed = 40000
+base_min_speed = 11000
+
 class ArmProcess(RoverProcess):
-	# Some blank space to write functions, classes, threads - whatever you need.
-	# There are no restrictions - this is your own process!
-
-	# See the tutorials on the wiki for things like basic threads and custom libraries.
-	# < TO DO: INSTERT LINK TO THAT WIKI AND WRITE IT >
-
-	# This is run once to set up anything you need.
-	# Hint: use the self object to store variables global to this process.
-	# You can also take any args you need from the main startup as a dictionary:
-	# self.something = args["something"]
-	# This can be handy for sharing semaphores with other processes!
     
 	# Subscribe ArmProcess to joystick keys for multiprocessing.
 	def setup(self, args):
-		for key in ["joystick1", "joystick2"]: # Add the keys to the subscription of the multiprocessor.
+		for key in ["joystick1", "joystick2", "Rtrigger"]: # Add the keys to the subscription of the multiprocessor.
 			self.subscribe(key)
+		self.base_direction = None
 
 	# Function that grabs the x and y axis values in message, then formats the data
 	#  and prints the result to stdout.
 	# Returns the newly formated x and y axis values in a new list
 	def on_joystick1(self, data):
 		x_axis = data[0] # Get data for x-axis.
-		x_axis = (x_axis * 40000/2) # Conversion factor to make it easy to interpret input, half power for testing.
-		if x_axis > 11000 or x_axis < -11000: # If out of dead-zone.
+		x_axis = (x_axis * base_max_speed/2) # Conversion factor to make it easy to interpret input, half power for testing.
+		if x_axis > base_min_speed or x_axis < -11000: # If out of dead-zone.
 			armBaseSpeed = x_axis
 		else:
 			armBaseSpeed = 0
 		self.log(armBaseSpeed, "DEBUG") # Print the status of this process.
 		self.publish("armBase", armBaseSpeed) # Publish the process to the multiprocessor.
+
+	def on_Rtrigger(self, trigger):
+		trigger = -1*(trigger + 1)/2
+		armBaseSpeed = trigger * base_max_speed/2
+		if self.base_direction is "right" or self.base_direction is None:
+			if -base_min_speed <armBaseSpeed < base_min_speed:
+				armBaseSpeed = 0
+				self.base_direction = None
+			else:
+				self.base_direction = "right"
+			self.log(armBaseSpeed, "DEBUG")
+			self.publish("armBase", pyvesc.SetRPM(armBaseSpeed)) # Publish the process to the multiprocessor.
+
+
+	def on_Ltrigger(self, trigger):
+		trigger = (trigger + 1)/2
+		armBaseSpeed = trigger * base_max_speed/2
+		if self.base_direction is "left" or self.base_direction is None:
+			if -base_min_speed <armBaseSpeed < base_min_speed:
+				armBaseSpeed = 0
+				self.base_direction = None
+			else:
+				self.base_direction = "left"
+			self.log(armBaseSpeed, "DEBUG")
+			self.publish("armBase", pyvesc.SetRPM(armBaseSpeed)) # Publish the process to the multiprocessor.
+
+
+
+
+
+
+
