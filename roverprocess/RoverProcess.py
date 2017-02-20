c109fcfee2ef9ef0daf4d47bee7a79a32de0049a
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-02-20 14:36:20 -0600

    Added some docstrings to RoverProcess

--- roverprocess/RoverProcess.py
+++ roverprocess/RoverProcess.py
@@ -18,33 +18,55 @@ import time
 import logging
 from collections import namedtuple
 
-# RoverMessage is a named tuple with key and data fields.
-# msg = RoverMessage('test', [1, 2, 3])
-# msg[0] == msg.key
-# msg[1] == msg.data
+''' RoverMessage is a named tuple with key and data fields.
+	msg = RoverMessage('test', [1, 2, 3])
+	msg[0] == msg.key
+	msg[1] == msg.data
+'''
 RoverMessage = namedtuple('RoverMessage', ['key', 'data'])
 
 
 class RoverProcess(Process):
+	""" RoverProcesses are modules that handle functionality of one component of the rover.
+		RoverProcesses somewhat emulate an Arduino, in that there is a setup and loop function.
+		They also provide an interprocess communication mechanism based on the publisher/subscriber
+		design pattern.
+	"""
+
 	class ReceiverThread(threading.Thread):
+		""" ReceiverThread listens for incoming messages."""
 		def __init__(self, downlink, parent):
+			""" Constructor. Required parameters:
+				downlink - The incomming message (multiprocessing) queue of the parent rover process.
+				parent   - instance of the RoverProcess
+			"""
 			threading.Thread.__init__(self)
-			self.downlink = downlink # MultiProcessing Queue.
-			self._parent = parent # RoverProcess instance.
+			self.downlink = downlink
+			self._parent = parent
 			self.quit = False
 			self.daemon = True
 
 		def run(self):
+			""" Run in a new thread. Loops until system shutdown.
+				Waits for a new message to come onto the downlink queue,
+				and if the RoverProcess has a method called on_<key>(), where
+				<key> is the key of the incomming RoverMessage, that method is called
+				with the message's data as a parameter. Otherwise, the RoverProcess's
+				messageTrigger method is called with the message as a parameter.
+			"""
 			while not self.quit:
-				message = self.downlink.get() # Get subscribed message from multiprocessing queue.
-				assert isinstance(message, RoverMessage) # Checking if the "message" is of type RoverMessage.
-				if hasattr(self._parent, "on_" + message.key): # If message key has a function called on_*key*() in its RoverProcess instance...
-					#...call trigger method, execute function.
-					getattr(self._parent, "on_" + message.key)(message.data) 
-				else: #... Otherwise call its message trigger.
+				message = self.downlink.get() # BLOCKING: Get subscribed message from multiprocessing queue.
+				assert isinstance(message, RoverMessage) # Ensure message is a RoverMessasge
+				if hasattr(self._parent, "on_" + message.key): # If the RoverProcess instance  has a function called on_<key>()...
+					getattr(self._parent, "on_" + message.key)(message.data)
+				else:
 					self._parent.messageTrigger(message)
 
 	def __init__(self, **kwargs):
+		""" Constructor, called in main.py automatically, don't override this through inheritance. Required parameters:
+			uplink   - queue for outgoing messages
+			downlink - queue for incomming messages
+		"""
 		Process.__init__(self)
 		self._log = logging.getLogger(self.__class__.__name__)
 		self.uplink = kwargs["uplink"]
@@ -57,6 +79,10 @@ class RoverProcess(Process):
 
 
 	def run(self):
+		""" This is method is what runs in a new process.
+		First the receiver thread is started, then the setup method is
+		called, and finally the loop method is run in an infinite while loop.
+		"""
 		self.receiver.start()
 		try:
 			self.setup(self._args)
@@ -75,26 +101,47 @@ class RoverProcess(Process):
 			raise
 
 	def setup(self, args):
+		""" Treated as a pseudo constructor, initialization of the process.
+			Arguments from the real constructor are passed in as <args>
+		"""
 		for msg_key in self.subscriptions:
 			self.subscribe(msg_key)
 
 	def loop(self):
+		""" Runs continually during the lifetime of the system.
+			Defaults to sleep for one second.
+		"""
 		try:
 			time.sleep(1)
 		except KeyboardInterrupt:
 			pass
 
 	def messageTrigger(self, message):
+		""" Generic message handler.
+			Typically, you want to use an if/else statement on the
+			message key and do different things depending on the key.
+		"""
 		pass
 
 	def on_quit(self, message):
+		''' Callback for the "quit" message.
+			Calls cleanup() and exits the process.
+		'''
 		self.cleanup()
 		sys.exit(0)
 
 	def publish(self, key, value):
+		""" Publishes a new RoverMessage to the rover system.
+			key - A string that serves as an identifier for the message.
+			value - The actual contents/data of the message.
+		"""
 		self.uplink.put(RoverMessage(key, value))
 
 	def cleanup(self):
+		""" Acts like a deconstructor.
+			Shuts down the receiver thread. If you override this method, be sure
+			to call RoverProcess.cleanup() to ensure everything shuts down properly.
+		"""
 		try:
 			if self.receiver != threading.current_thread():
 				logging.info(self.__class__.__name__ + " shutting down")
@@ -106,16 +153,29 @@ class RoverProcess(Process):
 			pass
 
 	def log(self, message, level="INFO"):
+		''' Logs a message.
+			Messages are logged to the console and a file, depending how things
+			are setup in main.py.
+			message - A string to log, can also be an integer, float etc.
+			level - A string representing the log level as defined in the standard python logging module.
+					Can be one of: "NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"
+		'''
 		level_lut = {"NOTSET":0, "DEBUG":10, "INFO":20,
 				"WARNING":30, "ERROR":40, "CRITICAL":50}
 		self._log.log(level_lut[level], message)
 
 	def subscribe(self, key):
+		""" Subscribe this RoverProcess to messages identified by <key>
+			key - A string identifier
+		"""
 		if key not in self.subscriptions:
 			self.subscriptions.append(key)
 		self.publish("subscribe", [key, self.__class__.__name__])
 
 	def unsubscribe(self, key):
+		""" Un-subscribe this RoverProcess to messages identified by <key>
+			key - A string identifier
+		"""
 		if key in self.subscriptions:
 			self.subscriptions.remove(key)
 		self.publish("unsubscribe", [key, self.__class__.__name__])
