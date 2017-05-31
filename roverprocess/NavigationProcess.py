ce250c94a0769e295a730fbd3fafd331ffa29ba6
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-05-30 19:49:27 -0600

    Added Compass reading to NavigationProcess

--- roverprocess/NavigationProcess.py
+++ roverprocess/NavigationProcess.py
@@ -3,8 +3,13 @@ from .RoverProcess import RoverProcess
 class NavigationProcess(RoverProcess):
 
     def setup(self, args):
-        self.subscribe("LidarDataMessage")
+        for msg in ["LidarDataMessage", "CompassDataMessage"]:
+            self.subscribe(msg)
 
     def on_LidarDataMessage(self, lidarmsg):
         self.log("Dist: "+str(lidarmsg.distance)+
                  " Angle: "+str(lidarmsg.angle))
+
+    def on_CompassDataMessage(self, msg):
+        self.log("heading: "+str(msg.heading))
+
