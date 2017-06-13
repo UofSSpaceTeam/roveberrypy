60314b5538d3fd3e87d2b7168be0b4b62233fcc9
Jarrod Pas <jarrodpas@gmail.com>
2017-06-13 02:32:26 -0600

    Write naive GPS based driving system

--- roverprocess/GPSDriveProcess.py
+++ roverprocess/GPSDriveProcess.py
@@ -1,65 +1,150 @@
-from math import asin, atan2, cos, radians, sin, sqrt
+from math import asin, atan2, cos, pi, radians, sin, sqrt
+from time import sleep
+
+from pyvesc import SetRPM
 
 from .RoverProcess import RoverProcess
 
-# haversine function
+# Haversine function
 hav = lambda z: (1 - cos(z)) / 2
 
-# inverse haversine function
+# Inverse haversine function
 ahav = lambda z: 2 * asin(sqrt(z))
 
+# Earth's radius in metres
 EARTH_RADIUS = 6371008.8
+
+# Maximum allowable distance for a target from current position in metres
+MAXIMUM_TARGET_DISTANCE = 10
+
+# Distance to consider target reached
+TARGET_CLOSE_DISTANCE = 1
+
+# TODO: Bearing error, what unit should this be in?
+BEARING_ERROR = 360
+
+# TODO: not sure where this number comes from but, it is min_rpm in
+# DriveProcess.py
+MOTOR_RPM = 2000
+
+# Delay for loop
+LOOP_DELAY_MS = 100
 
 class GPSPosition:
     def __init__(self, lat, lon):
+        # lat an lon are assumed to be in radians
         self.lat = lat
         self.lon = lon
 
     def distance(self, them):
         ''' Returns the distance to another GPSPositions on earth'''
-        s_lat, s_lon = self.radians()
-        t_lat, t_lon = them.radians()
+        d_lat = them.lat - self.lat
+        d_lon = them.lon - self.lon
 
-        d_lat = t_lat - s_lat
-        d_lon = t_lon - s_lon
-
-        z = hav(d_lat) + cos(s_lat)*cos(t_lat)*hav(d_lon)
+        z = hav(d_lat)
+            + cos(self.lat) * cos(them.lat) * hav(d_lon)
 
         return EARTH_RADIUS * ahav(z)
 
     def bearing(self, them):
         ''' Returns the bearing to another GPSPositions on earth'''
-        s_lat, s_lon = self.radians()
-        t_lat, t_lon = them.radians()
+        d_lat = them.lat - self.lat
+        d_lon = them.lon - self.lon
 
-        d_lat = t_lat - s_lat
-        d_lon = t_lon - s_lon
-
-        y = sin(d_lon)*cos(t_lat)
-        x = cos(s_lat)*sin(t_lat) - sin(s_lat)*cos(t_lat)*cos(d_lat)
+        y = sin(d_lon) * cos(them.lat)
+        x = cos(self.lat) * sin(them.lat)
+            - sin(self.lat) * cos(them.lat) * cos(d_lat)
 
         return atan2(y, x)
-
-    def radians(self):
-        '''Returns a tuple of latitude and longitude in radians'''
-        return (radians(self.lat), radians(self.lon))
 
 class GPSDriveProcess(RoverProcess):
     '''
     '''
+
     def setup(self, args):
-        self.subscribe('singlePointGPS')
-        self.current_position = None
-        self.target_position = None
-        self.last_position = None
+        self.target = None
+
+        self.position = None
+        self.position_last = None
+
+        self.heading = None
+        self.heading_last = None
+
+        self._rotating = False
+
+        for m in ['targetGPS', 'singlePointGPS', 'CompassDataMessage']:
+            self.subscribe(m)
+
+    def loop(self):
+        sleep(LOOP_DELAY_MS / 1000.0)
+
+        if self.target is None:
+            return
+
+        distance = self.position.distance(self.target)
+        bearing = self.position.bearing(self.target)
+
+        if distance < CLOSE_DISTANCE:
+            self.target = None
+            self._stop()
+            return
+
+        if abs(bearing) < CLOSE_BEARING and self._rotating:
+            self.rotating = False
+            self.forward()
+        else:
+            self.rotating = True
+            if bearing < 0:
+                self._turnLeft()
+            else:
+                self._turnRight()
+
+    def _setLeftWheelSpeed(rpm):
+        rpm = SetRPM(int(rpm))
+        self.publish("wheelLF", rpm)
+        self.publish("wheelLM", rpm)
+        self.publish("wheelLB", rpm)
+
+    def _setRightWheelSpeed(rpm):
+        rpm = SetRPM(int(rpm))
+        self.publish("wheelRF", rpm)
+        self.publish("wheelRM", rpm)
+        self.publish("wheelRB", rpm)
+
+    def _stop():
+        self._setLeftWheelSpeed(0)
+        self._setRightWheelSpeed(0)
+
+    def _forward():
+        self._setLeftWheelSpeed(MOTOR_RPM)
+        self._setRightWheelSpeed(MOTOR_RPM)
+
+    def _backward():
+        self._setLeftWheelSpeed(-MOTOR_RPM)
+        self._setRightWheelSpeed(-MOTOR_RPM)
+
+    def _turnRight():
+        self._setLeftWheelSpeed(MOTOR_RPM)
+        self._setRightWheelSpeed(-MOTOR_RPM)
+
+    def _turnLeft():
+        self._setLeftWheelSpeed(-MOTOR_RPM)
+        self._setRightWheelSpeed(MOTOR_RPM)
+
+    def on_targetGPS(self, pos):
+        '''Targets a new GPS coordinate'''
+        target = GPSPosition(radians(pos[0]), radians(pos[1]))
+
+        if target.distance(self.position) <= MAXIMUM_TARGET_DISTANCE:
+            self.target = target
+
+    def on_CompassDataMessage(self, compass):
+        '''Updates current heading'''
+        self.heading_last = self.heading
+        self.heading = compass.heading
 
     def on_singlePointGPS(self, pos):
-        self.last_position = self.current_position
-        self.current_position = GPSPosition(pos[0], pos[1])
-
-        if self.last_position is not None:
-            distance = self.last_position.distance(self.current_position)
-            bearing  = self.last_position.bearing(self.current_position)
-            self.log("distance: {}, bearing: {}".format(distance, bearing))
-
+        '''Updates GPS position'''
+        self.position_last = self.position
+        self.position = GPSPosition(radians(pos[0]), radians(pos[1]))
 
